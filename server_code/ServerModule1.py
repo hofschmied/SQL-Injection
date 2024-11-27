3c26401ac878a46dd700907a88519b12020bb01e
npfister.6822@gmail.com <npfister.6822@gmail.com>
2024-11-27 11:05:38 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,19 +1,21 @@
-import anvil.files
-from anvil.files import data_files
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
+import sqlite3
 import anvil.server
 
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
+# Verbindung zur SQLite-Datenbank herstellen
+def get_db_connection():
+    return sqlite3.connect('database.db')
+
+@anvil.server.callable
+def get_user_data(account_no=None):
+    db = get_db_connection()
+    cursor = db.cursor()
+    
+
+@anvil.server.callable
+def get_user(user_id):
+    db = get_db_connection()
+    cursor = db.cursor()
+    
+    cursor.execute("SELECT * FROM users WHERE AccountNo = ?", (user_id,))
+    user_id = cursor.fetchone()
+    db.close()
