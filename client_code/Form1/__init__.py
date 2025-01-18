01702c7ad9867a23c393e59f8a12050273006f1e
npfister.6822@gmail.com <npfister.6822@gmail.com>
2025-01-18 14:08:22 +0000

    Edited Form2 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,29 +1,36 @@
 from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.tables as tables
-import anvil.tables.query as q
+import anvil.tables.query as query
 from anvil.tables import app_tables
 import anvil.server
 
-
 class Form1(Form1Template):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-    if anvil.server.call('check_login_status'):
-      open_form('Form2')
-    else:
-      set_url_hash()
+    def __init__(self, **properties):
+        self.init_components(**properties)
+        user_logged_in = anvil.server.call('check_login_status')
+        if user_logged_in:
+            open_form('Form2')
+        else:
+            set_url_hash()
 
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    username = self.text_box_1.text
-    password = self.text_box_2.text
-    login_state = anvil.server.call("login", password, username, self.check_box_1.checked)
-    if not login_state[1]:
-      alert(login_state[0], title="Login Failed")
-    else:
-      res = anvil.server.call('getAccno', username, password)
-      if res is not None:
-        set_url_hash(f'?AccountNo={res}')
-      open_form('Form2')
+    def button_1_click(self, **event_args):
+        input_username = self.text_box_1.text
+        input_password = self.text_box_2.text
+        remember_me = self.check_box_1.checked
+
+        login_response = anvil.server.call("login", input_password, input_username, remember_me)
+        login_message, login_successful = login_response[0], login_response[1]
+
+        if not login_successful:
+            alert(login_message, title="Login Fehlgeschlagen")
+        else:
+            print("Login erfolgreich, weitere Überprüfung wird durchgeführt...")
+
+            account_no = anvil.server.call('getAccno', input_username, input_password)
+            if account_no is not None:
+                set_url_hash(f'?AccountNo={account_no}')
+            else:
+                print("Keine Kontonummer zurückgegeben. Fortsetzung des Login-Prozesses...")
+            open_form('Form2')
+
--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -5,19 +5,28 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-
 class Form2(Form2Template):
-  
-  def __init__(self, **properties):
-    self.init_components(**properties)
-    self.url = anvil.js.window.location.href
-    self.accNo = anvil.server.call('getQuery',self.url)
-    if (self.accNo == None):
-      self.label_1.text = "Login Successful but AccountNo was not passed."
-    else:
-      res = anvil.server.call('getUsrId', self.accNo)
-      self.label_1.text = res
 
-  def button_1_click(self, **event_args):
-    anvil.server.call('resetSession')
-    open_form('Form1')
+    def __init__(self, **properties):
+        self.init_components(**properties)
+        print("Form2 wird initialisiert...")
+
+        self.accNo = anvil.server.call('getQuery', self.url)
+      
+        self.url = anvil.js.window.location.href
+        print(f"Aktuelle URL: {self.url}")
+
+        if self.accNo is None:
+            self.label_1.text = "Login Successful but AccountNo was not passed."
+            print("Keine Kontonummer gefunden.")
+        else:
+            print(f"Kontonummer: {self.accNo}")
+            res = anvil.server.call('getUsrId', self.accNo)
+            self.label_1.text = res
+
+    def button_1_click(self, **event_args):
+        print("Logout wird ausgeführt...")
+        anvil.server.call('resetSession')
+        print("Sitzung wurde zurückgesetzt.")
+        open_form('Form1')
+        print("Form1 wurde geöffnet.")
